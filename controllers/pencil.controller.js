e9929bbb34ba9d3b5829c4c7ccfec0d576311431
jmroczka <jonathon.mroczka@gmail.com>
2018-11-30 16:58:47 -0500

    added whitespace tests to write function

--- controllers/pencil.controller.js
+++ controllers/pencil.controller.js
@@ -3,7 +3,17 @@ function write(paper, textToWrite) {
   if (typeof paper !== 'string') {
     return textToWrite;
   }
-  return paper.concat(textToWrite);
+  let newPaper;
+  if (paper.slice(-1) !== ' ' && textToWrite.slice(0, 1) !== ' ') {
+    newPaper = `${paper} ${textToWrite}`;
+    return newPaper;
+  }
+  if (paper.slice(-1) === ' ' && textToWrite.slice(0, 1) === ' ') {
+    newPaper = paper.slice(0, -1) + textToWrite;
+    return newPaper;
+  }
+  newPaper = paper + textToWrite;
+  return newPaper;
 }
 
 
--- tests/controllers/pencilController.test.js
+++ tests/controllers/pencilController.test.js
@@ -4,24 +4,44 @@ const PencilController = require('../../controllers/pencil.controller.js');
 const expect = chai.expect; // eslint-disable-line prefer-destructuring
 
 const VALID_PAPER_STRING = 'Boy, do I love me some bacon! What food do you like best?';
-const VALID_TEXT_TO_WRITE_STRING = ' I also really enjoy apples!';
+const VALID_WRITEABLE_STRING = ' I also really enjoy apples!';
+const VALID_WRITEABLE_STRING_MISSING_WHITESPACE = 'I also really enjoy apples!';
 const VALID_WRITE_RESULT = 'Boy, do I love me some bacon! What food do you like best? I also really enjoy apples!';
 
 describe('Pencil Controller', function () {
   describe('.write', function () {
     describe('if a valid paper is passed', function () {
       it('should append text to a valid paper string', function () {
-        const newPaper = PencilController.write(VALID_PAPER_STRING, VALID_TEXT_TO_WRITE_STRING);
+        const newPaper = PencilController.write(VALID_PAPER_STRING, VALID_WRITEABLE_STRING);
         expect(newPaper).to.equal(VALID_WRITE_RESULT);
+      });
+
+      describe('but is missing whitespace', function () {
+        it('should add whitespace to the paper in addition to new text to avoid errors', function () {
+          const newPaper = PencilController.write(
+            VALID_PAPER_STRING, VALID_WRITEABLE_STRING_MISSING_WHITESPACE,
+          );
+          expect(newPaper).to.equal(VALID_WRITE_RESULT);
+        });
+      });
+
+      describe('but the paper has trailing whitespace and new text has leading whitespace', function () {
+        it('should remove the extra whitespace then add the new text to avoid errors', function () {
+          const validPaperWithExtraWhitespace = `${VALID_PAPER_STRING} `;
+          const newPaper = PencilController.write(
+            validPaperWithExtraWhitespace, VALID_WRITEABLE_STRING,
+          );
+          expect(newPaper).to.equal(VALID_WRITE_RESULT);
+        });
       });
     });
 
     describe('if an invalid paper is passed', function () {
       it('should create a new paper and return the paper with the new text on it', function () {
-        const newPaperNull = PencilController.write(null, VALID_TEXT_TO_WRITE_STRING);
-        const newPaperInteger = PencilController.write(2, VALID_TEXT_TO_WRITE_STRING);
-        expect(newPaperNull).to.equal(VALID_TEXT_TO_WRITE_STRING);
-        expect(newPaperInteger).to.equal(VALID_TEXT_TO_WRITE_STRING);
+        const newPaperNull = PencilController.write(null, VALID_WRITEABLE_STRING);
+        const newPaperInteger = PencilController.write(2, VALID_WRITEABLE_STRING);
+        expect(newPaperNull).to.equal(VALID_WRITEABLE_STRING);
+        expect(newPaperInteger).to.equal(VALID_WRITEABLE_STRING);
       });
     });
   });
