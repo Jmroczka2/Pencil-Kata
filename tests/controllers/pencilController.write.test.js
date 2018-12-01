2a0c6fb2e36c56fcdec196942258a1635ae3ce47
jmroczka <jonathon.mroczka@gmail.com>
2018-12-01 16:52:05 -0500

    cleanup formatting of write tests

--- tests/controllers/pencilController.write.test.js
+++ tests/controllers/pencilController.write.test.js
@@ -10,85 +10,83 @@ const VALID_WRITE_RESULT = 'Boy, do I love me some bacon! What food do you like 
 const VALID_WRITE_RESULT_POINT_BREAK = 'Boy, do I love me some bacon! What food do you like best? I also real';
 const POINT_LEFT_ON_PENCIL = 10;
 
-describe('Pencil Controller', function () {
-  describe('.write()', function () {
-    describe('validity tests', function () {
-      beforeEach(function () {
-        PencilController.newPencil();
-      });
-      describe('if a valid paper is passed', function () {
-        it('should append text to a valid paper string', function () {
-          const newPaper = PencilController.write(VALID_PAPER_STRING, VALID_WRITEABLE_STRING);
-          expect(newPaper).to.equal(VALID_WRITE_RESULT);
-        });
-      });
-
-      describe('if an invalid paper is passed', function () {
-        it('should create a new paper and return the paper with the new text on it', function () {
-          const newPaperNull = PencilController.write(null, VALID_WRITEABLE_STRING);
-          const newPaperInteger = PencilController.write(2, VALID_WRITEABLE_STRING);
-          expect(newPaperNull).to.equal(VALID_WRITEABLE_STRING);
-          expect(newPaperInteger).to.equal(VALID_WRITEABLE_STRING);
-        });
+describe('PencilController.write()', function () {
+  describe('validity tests', function () {
+    beforeEach(function () {
+      PencilController.newPencil();
+    });
+    describe('if a valid paper is passed', function () {
+      it('should append text to a valid paper string', function () {
+        const newPaper = PencilController.write(VALID_PAPER_STRING, VALID_WRITEABLE_STRING);
+        expect(newPaper).to.equal(VALID_WRITE_RESULT);
       });
     });
 
-    describe('whitespace tests for paper and new text', function () {
-      beforeEach(function () {
-        PencilController.newPencil();
+    describe('if an invalid paper is passed', function () {
+      it('should create a new paper and return the paper with the new text on it', function () {
+        const newPaperNull = PencilController.write(null, VALID_WRITEABLE_STRING);
+        const newPaperInteger = PencilController.write(2, VALID_WRITEABLE_STRING);
+        expect(newPaperNull).to.equal(VALID_WRITEABLE_STRING);
+        expect(newPaperInteger).to.equal(VALID_WRITEABLE_STRING);
       });
-      describe('but the paper is missing whitespace', function () {
-        it('should add whitespace to the paper in addition to new text to avoid errors', function () {
-          const newPaper = PencilController.write(
-            VALID_PAPER_STRING, VALID_WRITEABLE_STRING_MISSING_WHITESPACE,
-          );
-          expect(newPaper).to.equal(VALID_WRITE_RESULT);
-        });
-      });
+    });
+  });
 
-      describe('but the paper has trailing whitespace', function () {
-        it('should remove the extra whitespace then add the new text to avoid errors', function () {
-          const validPaperWithExtraWhitespace = `${VALID_PAPER_STRING}       `;
-          const newPaper = PencilController.write(
-            validPaperWithExtraWhitespace, VALID_WRITEABLE_STRING,
-          );
-          expect(newPaper).to.equal(VALID_WRITE_RESULT);
-        });
-      });
-
-      describe('but the paper has trailing whitespace and new text has leading whitespace', function () {
-        it('should remove the extra whitespace then add the new text to avoid errors', function () {
-          const validPaperWithExtraWhitespace = `${VALID_PAPER_STRING} `;
-          const newPaper = PencilController.write(
-            validPaperWithExtraWhitespace, VALID_WRITEABLE_STRING,
-          );
-          expect(newPaper).to.equal(VALID_WRITE_RESULT);
-        });
+  describe('whitespace tests for paper and new text', function () {
+    beforeEach(function () {
+      PencilController.newPencil();
+    });
+    describe('if the paper is missing whitespace', function () {
+      it('should add whitespace to the paper in addition to new text to avoid errors', function () {
+        const newPaper = PencilController.write(
+          VALID_PAPER_STRING, VALID_WRITEABLE_STRING_MISSING_WHITESPACE,
+        );
+        expect(newPaper).to.equal(VALID_WRITE_RESULT);
       });
     });
 
-    describe('pencil point degradation tests', function () {
-      describe('if the pencil has no point left', function () {
-        beforeEach(function () {
-          PencilController.pencil.point = 0;
-        });
-        it('should write nothing to the paper', function () {
-          const newPaper = PencilController.write(VALID_PAPER_STRING, VALID_WRITEABLE_STRING);
-          expect(newPaper).to.equal(VALID_PAPER_STRING);
-        });
+    describe('if the paper has trailing whitespace', function () {
+      it('should remove the extra whitespace then add the new text to avoid errors', function () {
+        const validPaperWithExtraWhitespace = `${VALID_PAPER_STRING}       `;
+        const newPaper = PencilController.write(
+          validPaperWithExtraWhitespace, VALID_WRITEABLE_STRING,
+        );
+        expect(newPaper).to.equal(VALID_WRITE_RESULT);
       });
+    });
 
-      describe('if the pencil has some point but not enough to write all the text', function () {
-        before(function () {
-          PencilController.pencil.point = POINT_LEFT_ON_PENCIL;
-        });
-        it('should use the point that it has to write what text it can to the paper', function () {
-          const newPaper = PencilController.write(VALID_PAPER_STRING, VALID_WRITEABLE_STRING);
-          expect(newPaper).to.equal(VALID_WRITE_RESULT_POINT_BREAK);
-        });
-        it('should leave the pencil with no point left', function () {
-          expect(PencilController.pencil.point).to.be.lte(0);
-        });
+    describe('if the paper has trailing whitespace and new text has leading whitespace', function () {
+      it('should remove the extra whitespace then add the new text to avoid errors', function () {
+        const validPaperWithExtraWhitespace = `${VALID_PAPER_STRING} `;
+        const newPaper = PencilController.write(
+          validPaperWithExtraWhitespace, VALID_WRITEABLE_STRING,
+        );
+        expect(newPaper).to.equal(VALID_WRITE_RESULT);
+      });
+    });
+  });
+
+  describe('pencil point degradation tests', function () {
+    describe('if the pencil has no point left', function () {
+      beforeEach(function () {
+        PencilController.pencil.point = 0;
+      });
+      it('should write nothing to the paper', function () {
+        const newPaper = PencilController.write(VALID_PAPER_STRING, VALID_WRITEABLE_STRING);
+        expect(newPaper).to.equal(VALID_PAPER_STRING);
+      });
+    });
+
+    describe('if the pencil has some point but not enough to write all the text', function () {
+      before(function () {
+        PencilController.pencil.point = POINT_LEFT_ON_PENCIL;
+      });
+      it('should use the point that it has to write what text it can to the paper', function () {
+        const newPaper = PencilController.write(VALID_PAPER_STRING, VALID_WRITEABLE_STRING);
+        expect(newPaper).to.equal(VALID_WRITE_RESULT_POINT_BREAK);
+      });
+      it('should leave the pencil with no point left', function () {
+        expect(PencilController.pencil.point).to.be.lte(0);
       });
     });
   });
